b0a757296b34abfaff510f2773ef6561b02091c1
skronschlaeger16 <sebkro90@gmail.com>
2020-04-27 22:15:56 +0200

    Backend next Commit2

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO employees (id, latitude, longitude, name) VALUES (1, '120', '123', 'Sebastian');
-INSERT INTO employees (id, latitude, longitude, name) VALUES (2, '7656', '4891', 'Kronschläger');
-INSERT INTO employees (id, latitude, longitude, name) VALUES (3, '7434', '63636', 'JOOOO');
+INSERT INTO employees (id, latitude, longitude, name) VALUES (1, '48.3438653', '13.7710445', 'Sebastian');
+INSERT INTO employees (id, latitude, longitude, name) VALUES (2, '35.6828387', '139.7594549', 'Lukas');
+INSERT INTO employees (id, latitude, longitude, name) VALUES (3, '35.6828387', '139.7594549', 'Florian');
 
-INSERT INTO services (id, datee, employee_id, latitude, longitude, name) VALUES (1, '21.01.2019 11:12', 1, '4894', '45486', 'Service1');
-INSERT INTO services (id, datee, employee_id, latitude, longitude, name) VALUES (2, '21.01.2019 11:12', 2, '894', '123', 'Service2');
+INSERT INTO services (id, datee, employee_id, latitude, longitude, name) VALUES (1, '4/3/2002', 1, '48.3438653', '13.7710445', 'Putzen');
+INSERT INTO services (id, datee, employee_id, latitude, longitude, name) VALUES (2, '27/4/2019', 2, '35.6828387', '139.7594549', 'Kochen');
